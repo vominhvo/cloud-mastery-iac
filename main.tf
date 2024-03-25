5f2f678db4d81888dd627312ed088d2939772805
tmtam612 <37206193+tmtam612@users.noreply.github.com>
2024-03-25 10:25:58 +0700

    Update main.tf

--- main.tf
+++ main.tf
@@ -32,63 +32,6 @@ resource "azurerm_user_assigned_identity" "user_assigned_identity" {
 #   virtual_ip_address  = var.virtual_ip_address
 # }
 
-# config cosmosDB
-# module "cosmosDB" {
-#   source                            = "./modules/cosmos-db"
-#   project_name                      = local.project_name
-#   environment                       = local.environment
-#   location                          = local.location
-#   instance_count                    = local.instance_count
-#   resource_group_name               = azurerm_resource_group.this.name
-#   azurerm_user_assigned_identity_id = azurerm_user_assigned_identity.user_assigned_identity.id
-#   combined_vars                     = var.cosmosdb_combined_vars
-# }
-
-#config keyvault
-# module "key_vault" {
-#   source              = "./modules/key-vaults"
-#   project_name        = local.project_name
-#   environment         = local.environment
-#   location            = local.location
-#   instance_count      = local.instance_count
-#   resource_group_name = azurerm_resource_group.this.name
-#   tenant_id           = data.azurerm_client_config.current.tenant_id
-#   object_id           = data.azurerm_client_config.current.object_id
-#   combined_vars       = var.key_vault_combined_vars
-#   key_opts            = var.key_opts
-#   key_permissions     = var.key_permissions
-#   secret_permissions  = var.secret_permissions
-#   storage_permissions = var.storage_permissions
-# }
-
-# config service bus
-# module "service_bus" {
-#   source              = "./modules/service-bus"
-#   project_name        = local.project_name
-#   environment         = local.environment
-#   location            = local.location
-#   instance_count      = local.instance_count
-#   resource_group_name = azurerm_resource_group.this.name
-#   combined_vars       = var.service_bus_combined_vars
-# }
-
-#config account service
-# module "account_service" {
-#   source                    = "./modules/app-services"
-#   project_name              = local.project_name
-#   environment               = local.environment
-#   location                  = local.location
-#   instance_count            = local.instance_count
-#   resource_group_name       = azurerm_resource_group.this.name
-#   user_assigned_identity_id = azurerm_user_assigned_identity.user_assigned_identity.id
-#   cosmosdb_account_id       = ""
-#   # cosmosdb_account_id           = module.cosmosDB.cosmosdb_account_id
-#   combined_vars                 = var.account_app_service_combined_vars
-#   service_plan_id               = ""
-#   service_bus_connection_string = module.service_bus.primary_connection_string
-#   subnet_id                     = module.network.subnet1_id
-# }
-
 #config k8s cluster
 module "kubernetes" {
   source                 = "./modules/kubernetes"
@@ -99,11 +42,3 @@ module "kubernetes" {
   combined_vars          = var.aks_combined_vars
   user_assigned_identity = azurerm_user_assigned_identity.user_assigned_identity.id
 }
-# module "k8s" {
-#   source                 = "./modules/k8s"
-#   host                   = module.kubernetes.host
-#   client_certificate     = base64decode(module.kubernetes.client_certificate)
-#   client_key             = base64decode(module.kubernetes.client_key)
-#   cluster_ca_certificate = base64decode(module.kubernetes.cluster_ca_certificate)
-#   github_token           = var.github_token
-# }
